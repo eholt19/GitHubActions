a6841a513ee23180f82ce000d130917916120acb
Emily <eholt179@gmail.com>
2025-05-20 01:59:18 -0600

    last test of the night

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    // Tell Cypress where your spec files are:
+    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(_on, _config) {
-      // node event listeners
+      // Node event listeners
     },
   },
 });
